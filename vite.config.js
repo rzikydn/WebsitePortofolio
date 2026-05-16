1ed03b171317c679f8d07c6a61c49a116c8f9c1e
Rzky Wildan <rzkywildan12@example.com>
2026-05-16 15:55:01 +0700

    V9

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 15'],
+      polyfills: true,
+    })
+  ],
+  assetsInclude: ['**/*.glb'],
+  build: {
+    target: 'es2015',
+  }
 })
